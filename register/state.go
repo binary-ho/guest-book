4267986a5d56bdc1eb6ec279d0fbb2e564e12463
binary_ho <dfghcvb11@naver.com>
2024-06-10 12:35:06 +0900

    refactor : Merge시 timestamp, plan, id를 비교하도록 변경 (#6)

--- register/state.go
+++ register/state.go
@@ -7,6 +7,38 @@ import (
 
 type State struct {
 	user      entity.User
-	timestamp uint
-	value     rgb.RGB
+	timestamp uint32
+	color     rgb.RGB
+}
+
+func (state *State) SetColor(rgb rgb.RGB) {
+	state.color = rgb
+}
+
+func (state *State) isWin(remoteState *State) bool {
+	if isTimestampBigger(state, remoteState) ||
+		isPlanHigher(state, remoteState) ||
+		isIdLower(state, remoteState) {
+		return true
+	}
+	return false
+}
+
+func isTimestampBigger(state *State, remoteState *State) bool {
+	return state.timestamp > remoteState.timestamp
+}
+
+func isPlanHigher(state *State, remoteState *State) bool {
+	plan := state.user.Plan()
+	remotePlan := remoteState.user.Plan()
+	return state.timestamp == remoteState.timestamp &&
+		plan.IsHigher(&remotePlan)
+}
+
+func isIdLower(state *State, remoteState *State) bool {
+	plan := state.user.Plan()
+	remotePlan := remoteState.user.Plan()
+	return state.timestamp == remoteState.timestamp &&
+		plan.IsEquals(&remotePlan) &&
+		state.user.Id() < remoteState.user.Id()
 }
