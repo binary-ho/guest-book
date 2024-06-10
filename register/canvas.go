ef8ba1e1063d8f9df456b48ebfbc05a8a0de14a2
binary_ho <dfghcvb11@naver.com>
2024-06-11 00:41:33 +0900

    refactor : 좌표 변환 key값을 uint32로 변경 (#7)

--- register/canvas.go
+++ register/canvas.go
@@ -4,10 +4,10 @@ import "guestbook/entity"
 
 type Canvas struct {
 	id   entity.ID
-	data map[string]LWWRegister
+	data map[uint32]LWWRegister
 }
 
-func (llwMap Canvas) IsExist(key string) bool {
+func (llwMap Canvas) IsExist(key uint32) bool {
 	_, exist := llwMap.data[key]
 	return exist
 }
