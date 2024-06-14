5c5946ad7c4c8fffe1b500a10c02df823a75e883
binary_ho <dfghcvb11@naver.com>
2024-06-14 16:20:13 +0900

    refactor : 캔버스 좌표, 축, Register Map과 Key값을 Type으로 Wrapping (#21)

---
+++ register/colors.go
@@ -0,0 +1,10 @@
+package register
+
+/*
+TODO : (0, 0) 부터 시작할지 (1, 1) 부터 시작할지?
+(0, 0)은 기본값이므로 문제가 생길 수도 있음.
+일단은 (0, 0)으로 한다. (1, 1)의 경우 값을 클라이언트로 내려줄 때 까다로욱 수도 있음
+*/
+type colors map[colorKey]LWWRegister
+
+type colorKey uint32
