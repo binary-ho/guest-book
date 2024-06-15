5c1dcaea55f7a9c329be00b7991d37d621f6e59f
binary_ho <dfghcvb11@naver.com>
2024-06-15 19:52:01 +0900

    feat : 소켓 통신이 이루어지는 Room과 Client, Client Map인 Clients 구현 (#11)

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module guestbook
 
 go 1.22
+
+require (
+	github.com/gorilla/websocket v1.5.2 // indirect
+	golang.org/x/net v0.23.0 // indirect
+)
---
+++ room/drawing-room.go
@@ -0,0 +1,57 @@
+package room
+
+import "guestbook/entity"
+
+type DrawingRoom struct {
+	canvasId entity.ID
+	inbox    chan Message
+	join     chan *Client
+	leave    chan *Client
+	clients  Clients
+}
+
+func OpenDrawingRoom(canvasId entity.ID, client *Client) *DrawingRoom {
+	room := &DrawingRoom{
+		canvasId: canvasId,
+		inbox:    make(chan Message),
+		join:     make(chan *Client),
+		leave:    make(chan *Client),
+		clients:  make(Clients),
+	}
+
+	room.clients.Insert(client)
+	// TODO : run을 돌리면서 room 정보를 반환하는 방법?
+	room.run()
+	return room
+}
+
+func (room *DrawingRoom) run() {
+	for {
+		select {
+		case client := <-room.join:
+			room.clients.Insert(client)
+		case client := <-room.leave:
+			room.clients.Remove(client)
+			close(client.inbox)
+			// TODO : 아직 메시지가 남아있는 경우 Room을 즉시 닫지 않을 수 있을까?
+			if room.isEmpty() {
+				room.closeRoom()
+				return
+			}
+		case message := <-room.inbox:
+			for client := range room.clients {
+				client.inbox <- message
+			}
+		}
+	}
+}
+
+func (room *DrawingRoom) isEmpty() bool {
+	return room.clients.IsEmpty()
+}
+
+func (room *DrawingRoom) closeRoom() {
+	close(room.inbox)
+	close(room.join)
+	close(room.leave)
+}
