8b361ff6ccef2d8d167cb4ef74ce29abdee196d5
binary_ho <dfghcvb11@naver.com>
2024-06-18 12:33:48 +0900

    feat : Register 생성 메서드 추가 (#12)

--- register/register.go
+++ register/register.go
@@ -4,12 +4,17 @@ type LWWRegister struct {
 	state *State
 }
 
-func (register *LWWRegister) Merge(remoteState *State) {
+func CreateLLWWRegister(state *State) *LWWRegister {
+	return &LWWRegister{state}
+}
+
+func (register *LWWRegister) Merge(remoteState *State) *LWWRegister {
 	if remoteState == nil {
-		return
+		return register
 	}
 
 	if state := *register.state; state.isWin(remoteState) {
 		state.SetColor(remoteState.color)
 	}
+	return register
 }
