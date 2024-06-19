c62b6f864d0941ec7cd84c9364d56a7c4ef48688
binary_ho <dfghcvb11@naver.com>
2024-06-19 13:02:30 +0900

    feat : structured server error logging (#24)

---
+++ cmd/error.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func (app *application) internal(writer http.ResponseWriter, request *http.Request, err error) {
+	var (
+		method = request.Method
+		uri    = request.URL.RequestURI
+	)
+
+	app.logger.Error("[INTERNAL ERROR] : ", err.Error(), ", method: ", method, ", uri: ", uri)
+	internalError := http.StatusInternalServerError
+	http.Error(writer, http.StatusText(internalError), internalError)
+}
+
+func (app *application) client(writer http.ResponseWriter, status int) {
+	statusText := http.StatusText(status)
+	if isBlank(statusText) {
+		app.logger.Error("[CLIENT ERROR] STATUS WRONG -> " + strconv.Itoa(status))
+	}
+	app.logger.Error("[CLIENT ERROR] status : ", status)
+	http.Error(writer, statusText, status)
+}
+
+func isBlank(str string) bool {
+	return str == ""
+}
