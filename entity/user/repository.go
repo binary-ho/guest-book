2ecc782455c4a91687fdb8cb9dd2b493233fc1ca
binary_ho <dfghcvb11@naver.com>
2024-06-21 00:54:13 +0900

    feat : User Repository 구현. 기본 FindById, ExistsByGithubId, Save (#12)

---
+++ entity/user/repository.go
@@ -0,0 +1,66 @@
+package user
+
+import (
+	"database/sql"
+	"errors"
+	"guestbook/entity"
+)
+
+type RepositoryInterface interface {
+	FindById(entity.ID) (*Entity, error)
+	ExistsByGithubId(entity.ID) bool
+	Save(Entity) error
+}
+
+type Repository struct {
+	DB *sql.DB
+}
+
+const (
+	SELECT_USER_BY_ID        = `SELECT id, handle, nickname, githubId, profileUrl, plan FROM users WHERE id = ?`
+	SELECT_USER_BY_GITHUB_ID = `SELECT id, handle, nickname, githubId, profileUrl, plan FROM users WHERE githubId = ?`
+	INSERT_USER              = `INSERT INTO users (id, handel, nickname, githubId, profileUrl, plan) VALUES (?, ?, ?, ?, ?, ?)`
+	UPDATE_USER              = `UPDATE users SET handle = ?, nickname = ?, profileUrl = ?, githubId = ?, plan = ? WHERE id = ?`
+)
+
+func (repo *Repository) FindById(id entity.ID) (*Entity, error) {
+	row := repo.DB.QueryRow(SELECT_USER_BY_ID, id)
+	return scanRow(row)
+}
+
+func (repo *Repository) ExistsByGithubId(githubId entity.ID) bool {
+	row := repo.DB.QueryRow(SELECT_USER_BY_GITHUB_ID, githubId)
+	_, err := scanRow(row)
+	return err == nil || !errors.Is(err, sql.ErrNoRows)
+}
+
+func (repo *Repository) Save(user Entity) (*Entity, error) {
+	if user.ID() == entity.DefaultId() {
+		return insertUser(user, repo)
+	}
+	_, err := repo.DB.Exec(UPDATE_USER, user.handle, user.nickname, user.githubId, user.profileUrl, user.plan)
+	return &user, err
+}
+
+func insertUser(user Entity, repo *Repository) (*Entity, error) {
+	result, err := repo.DB.Exec(
+		INSERT_USER, user.id, user.handle, user.nickname, user.githubId, user.profileUrl, user.plan)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := result.LastInsertId()
+	return &Entity{
+		id:         entity.ID(id),
+		handle:     user.handle,
+		nickname:   user.nickname,
+		githubId:   user.githubId,
+		profileUrl: user.profileUrl,
+		plan:       user.Plan(),
+	}, nil
+}
+
+func scanRow(row *sql.Row) (*Entity, error) {
+	var user Entity
+	return &user, row.Scan(&user.id, &user.handle, &user.nickname, &user.githubId, &user.profileUrl, &user.plan)
+}
